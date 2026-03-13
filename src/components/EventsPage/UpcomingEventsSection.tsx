f0d1dd70659e7e4bfa93d8987ca3f9383e807b59
James Karnes <karnes.james@gmail.com>
2026-03-13 13:50:05 -0400

    added individual events to sanity and fetching cards

--- src/components/EventsPage/UpcomingEventsSection.tsx
+++ src/components/EventsPage/UpcomingEventsSection.tsx
@@ -44,7 +44,7 @@ export const UpcomingEventsSection = ({
         {filteredEvents.length > 0 ? (
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
             {filteredEvents.map(event => (
-              <EventCard key={event.id} event={event} />
+              <EventCard key={event.slug.current} event={event} />
             ))}
           </div>
         ) : (
